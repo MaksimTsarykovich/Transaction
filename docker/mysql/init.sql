e64dd88c8710dfee808ab5c47d9f0cd937a9e068
MaksimTsarykovich <maksimtsarykovich@gmail.com>
2025-02-06 16:09:48 +0300

    добавил автоматическое создание таблицы при поднятии докера

---
+++ docker/mysql/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE transactions
+(
+    `id`          INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `date`        VARCHAR(55)                    NOT NULL,
+    `check`   VARCHAR(55),
+    `description` VARCHAR(255) UNIQUE            NOT NULL,
+    `amount`      INT                            NOT NULL,
+    `is_positive` BOOL                           NOT NULL
+);
